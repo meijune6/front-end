1198554876552945ece44dcbb21b9423c51479e3
dhio putera <nymses6@gmail.com>
2023-10-15 21:20:07 +0700

    first commit

---
+++ app/About/page.js
@@ -0,0 +1,6 @@
+
+export default function About() {
+  return (
+    <h1 className='mt-20'>Halaman About</h1>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ul className='flex'>
+          <li className='mr-2'><Link href='/'>Home</Link></li>
+          <li><Link href='/About'>About</Link></li>
+        </ul>
+        {children}
+      </body>
     </html>
   )
 }
